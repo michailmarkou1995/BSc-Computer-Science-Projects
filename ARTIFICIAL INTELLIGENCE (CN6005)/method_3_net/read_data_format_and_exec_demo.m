abff49307288297d7623f1560e63aa0d390958f7
michail <backtrackpower@gmail.com>
2021-12-18 11:46:23 +0200

    mse and re-train
    success calcs done

--- ARTIFICIAL INTELLIGENCE (CN6005)/method_3_net/read_data_format_and_exec_demo.m
+++ ARTIFICIAL INTELLIGENCE (CN6005)/method_3_net/read_data_format_and_exec_demo.m
@@ -35,86 +35,104 @@ t = t'; % or t = t.';
 %load ('wdbc.mat');
 
 %% Prepare Neural Network
-hidden_layers_neurons = [10];
-training_function = 'traingdx'; %trainscg trainrp  traingdx
-net = patternnet(hidden_layers_neurons); %feedforwardnet
-%net = setwb(net,rand(55,1));
-%net = setwb(net,1);
-%net.IW{1,1}
-%net.b{1}
-%net.b = [2 1; 1];
-%view(net);
-%net.divideFcn = 'dividerand';
-net.trainFcn = training_function;
-%net.trainParam.goal = 1e-2;
-net.trainParam.epochs = 1500;
-net.trainParam.show = 1;
-net.performFcn = 'mse';
 
-net.divideParam.trainRatio = 70/100;
-net.divideParam.valRatio = 15/100;
-net.divideParam.testRatio = 15/100;
+for test_various_training_functions_average_results = 1:3
+    hidden_layers_neurons = [10];
+    if (test_various_training_functions_average_results == 1)
+        training_function = 'trainscg';
+    end
+    if (test_various_training_functions_average_results == 2)
+        training_function = 'trainrp';
+    end
+    if (test_various_training_functions_average_results == 3)
+        training_function = 'traingdx';
+    end
+    
+    net = patternnet(hidden_layers_neurons); %feedforwardnet
+    %net = setwb(net,rand(55,1));
+    %net = setwb(net,1);
+    %net.IW{1,1}
+    %net.b{1}
+    %net.b = [2 1; 1];
+    %view(net);
+    %net.divideFcn = 'dividerand';
+    net.trainFcn = training_function;
+    %net.trainParam.goal = 1e-2;
+    net.trainParam.epochs = 1500;
+    net.trainParam.show = 1;
+    net.performFcn = 'mse'; % crossentropy
 
-net.trainParam.max_fail = 10;
+    net.divideParam.trainRatio = 70/100;
+    net.divideParam.valRatio = 15/100;
+    net.divideParam.testRatio = 15/100;
 
-% traingdx only
-net.trainParam.lr = 0.1; %0.1 0.4;
-net.trainParam.mc = 0.4; %0.4 0.9;
+    net.trainParam.max_fail = 10;
 
-net = init(net);
+    % traingdx only
+    if (strcmp(training_function, 'traingdx'))     
+        net.trainParam.lr = 0.1; %0.1 0.4;
+        net.trainParam.mc = 0.4; %0.4 0.9;
+    end
 
-x = x(1:569,1:10);
- x = x';
- t=t';
-[net,tr] = train(net, x, t);
+    net = init(net);
 
-%% Confusion Matrix
+    if (test_various_training_functions_average_results == 1)
+         x = x(1:569,1:10);
+         x = x';
+         t=t';
+    end
 
-%outputs = net(x); % target expected - output that got from test data
-outputs = zeros(length(x(:,:)),length(x(1,:)));%zeros(1,size(x(1:1,1:1)));
-pcorrect=0;
-perror=0;
-%myGhostFigure = figure("Visible",false);
-%confmatrix = figure, plotconfusion(t, outputs);
-%close(findall(groot,'Type','figure'))
-%shoi = confmatrix.CurrentAxes.Children(4).String;
-%shoi = regexprep(shoi,'[^?:\.A-Za-z1-9]',''); % keep only numbers with dot but not 0 with .
-%shoi_n = str2double(shoi); %shoi_n = str2num(['uint8(',shoi,')']);
+    [net,tr] = train(net, x, t); %train
 
-% once total percentage of Confusion Matrix correctness
-%figure, plotperform(tr);
-%figure, plotconfusion(t, outputs);
-%[c,cm] = confusion(t,outputs);
-%fprintf('Percentage Correct Classification   : %f%%\n', 100*(1-c));
-%fprintf('Percentage Incorrect Classification : %f%%\n', 100*c);
-%plotroc(t,outputs);
+    %% Confusion Matrix
 
-% Average re-train confusion
-for i=1:100
-    outputs = net(x);
-    [c,cm,ind,per] = confusion(t,outputs);
-    pcorrect = pcorrect + (100*(1-c));
-    perror = perror + (100*c);
-    if (i==100)
-        pcorrect = pcorrect / 100;
-        perror = perror / 100;
-        fprintf('Average Percentage Correct Classification   : %f%%\n', pcorrect);
-        fprintf('Average Percentage Incorrect Classification : %f%%\n', perror);
+    outputs = zeros(length(x(:,:)),length(x(1,:)));
+    pcorrect=0;
+    perror=0;
+
+    % Average re-train confusion
+    mse=0.0;
+    for i=1:15
+        [net,tr] = train(net, x, t); % re-train
+        outputs = net(x);
+        Nepochs = tr.epoch(end); % Total epochs iteration of net train
+        N = length(x); % Proccessed input Data Length
+        for i_epoch = 1:Nepochs % iterations run (total number of epochs trained)
+            for i_mse = 1:N % do addititive summary 569 times row (1 epoch) then divide it with 569.. do that for Nepochs
+                 mse(1) = mse(1) + mean((t(i_mse)-outputs(i_mse)).^2); % add in first col the previous mse + next the row-by-row actual target from Data - Data from output you got instead
+            end
+            mse(1) = mse(1)/N;
+        end
+        [c,cm,ind,per] = confusion(t,outputs);
+        pcorrect = pcorrect + (100*(1-c));
+        perror = perror + (100*c);
+        if (i==15)
+            pcorrect = pcorrect / 15;
+            perror = perror / 15;
+            fprintf('%s = Average Percentage Total Correct Classification   : %f%% | MSE : %f%%\n', training_function, pcorrect, mse(1));
+            fprintf('%s = Average Percentage Total Incorrect Classification : %f%%\n', training_function, perror);
+        end
     end
 end
 
-%% CALCULATE MSE
-Nepochs = tr.epoch(end); % Total epochs iteration of net train
-N = length(x); % Proccessed input Data Length
-for i_epoch = 1:Nepochs % iterations run (total number of epochs trained)
-    for i_mse = 1:N %569
-         mse(1) = mse(1) + mean((t(i_mse)-outputs(i_mse)).^2); % rmse or mse? % add in first col the previous mse + next the row-by-row actual target from Data - Data from output you got instead
-    end
-    mse(1) = mse(1)/N;
-end
+%% Draft Area
 
-for i = 1:N-1
-    %mse(i) = mean((y(i)-x(i)).^2)
-    mse_graph(i) = mean((t(i)-outputs(i)).^2);
-end
+% for i = 1:N-1
+%     mse_graph(i) = mean((t(i)-outputs(i)).^2);
+% end
 %plot(abs(mse_graph));
+
+%shoi = regexprep(shoi,'[^?:\.A-Za-z1-9]',''); % keep only numbers with . start from 1
+%shoi_n = shoi_n + str2double(shoi);
+
+% % CALCULATE MSE
+% mse=0.0;
+% Nepochs = tr.epoch(end); % Total epochs iteration of net train
+% N = length(x); % Proccessed input Data Length
+% for i_epoch = 1:Nepochs % iterations run (total number of epochs trained)
+%     for i_mse = 1:N % do addititive summary 569 times row (1 epoch) then divide it with 569.. do that for Nepochs
+%          mse(1) = mse(1) + mean((t(i_mse)-outputs(i_mse)).^2); % add in first col the previous mse + next the row-by-row actual target from Data - Data from output you got instead
+%     end
+%     mse(1) = mse(1)/N;
+% end
+% fprintf('last mse output train : %f%%\n', mse(1));
